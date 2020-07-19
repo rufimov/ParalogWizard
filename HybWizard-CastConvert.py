273c25bd0e3427abdb233a4d0ec320837d20463f
rufimov <r.ufimov@gmail.com>
2020-07-19 11:37:34 +0200

    Fix a function

--- HybWizard-CastConvert.py
+++ HybWizard-CastConvert.py
@@ -70,16 +70,14 @@ def slicing(dictionary: Dict[str, Bio.SeqRecord.SeqRecord], current_string: str,
                                          -int(current_string.split()[end_column]) - 1:-1][::-1]
 
 
-def intersect_of_equal_length(str1, str2, mode):
-    lenmax = max(len(str1), len(str2))
-    match_ids = []
-    for i in range(lenmax):
-        if str1[i] != '-' and str2[i] != '-':
-            match_ids.append(i)
-    if mode == 'len':
-        return match_ids[-1] - match_ids[0] + 1
-    elif mode == 'ids':
-        return match_ids[0], match_ids[-1]
+def intersect_of_equal_length(str1, str2):
+    while (str1.startswith('-') or str2.startswith('-')) and str1[0] != str2[0]:
+        str1 = str1[1:]
+        str2 = str2[1:]
+    while (str1.endswith('-') or str2.endswith('-')) and str1[0] != str2[0]:
+        str1 = str1[:-1]
+        str2 = str2[:-1]
+    return len(str1)
 
 
 def percent_dissimilarity(seq1: str, seq2: str) -> float:
