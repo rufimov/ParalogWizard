50c5a07f6bb6688c013557af11d2303dff6e62d1
rufimov <r.ufimov@gmail.com>
2020-07-24 18:28:02 +0200

    Minor optimizations

--- HybWizard_CastAnalyze.py
+++ HybWizard_CastAnalyze.py
@@ -49,6 +49,16 @@ def estimate_divergence():
         for i in divergency_distribution:
             divergency_distribution_to_write.write(str(i) + '\n')
     divergency_distribution_array: numpy.ndarray = numpy.array([[x] for x in divergency_distribution])
+    divergency_distribution_mix: BayesianGaussianMixture = get_best_model(divergency_distribution_array, 1)
+    means: List[float] = divergency_distribution_mix.means_.flatten().tolist()
+    sigmas: List[float] = [numpy.sqrt(x) for x in divergency_distribution_mix.covariances_.flatten().tolist()]
+    mu_sigma: List[Tuple[float, float]] = sorted(list(zip(means, sigmas)), key=lambda x: x[0])
+    first_peak: float = mu_sigma[0][0]
+    first_peak_minus_sigma: float = first_peak - mu_sigma[0][1]
+    get_plot(path_to_data_HPM + '/exons/aln_orth_par/', 'pairwise_distances_distribution_1_comp',
+             divergency_distribution_array, divergency_distribution_mix, {
+                 'first peak minus sigma ': numpy.round(first_peak_minus_sigma, 2),
+                 'first peak ': numpy.round(first_peak, 2)}, '', 'individual')
     divergency_distribution_mix: BayesianGaussianMixture = get_best_model(divergency_distribution_array, 2)
     means: List[float] = divergency_distribution_mix.means_.flatten().tolist()
     sigmas: List[float] = [numpy.sqrt(x) for x in divergency_distribution_mix.covariances_.flatten().tolist()]
