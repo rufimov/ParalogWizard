b0f627957d3865e538d3d179f3e58f114316ddff
rufimov <r.ufimov@gmail.com>
2020-11-16 19:24:20 +0100

    Bug fix

--- ParalogWizard_2b_CastSubmitCreate.sh
+++ ParalogWizard_2b_CastSubmitCreate.sh
@@ -45,6 +45,7 @@ cd "${SCRATCHDIR}" || exit 1
 #Copy scripts and reference to scratch
 cp "${source}"/ParalogWizard_CastCreate.py .
 cp "${source}"/ParalogWizard_Functions.py .
+cp "${source}/${probe_HP_exons_split}" .
 
 env echo
 
@@ -52,7 +53,7 @@ env echo 'Running script...'
 env echo
 
 
-python3 ParalogWizard_CastCreate.py "${path_to_data_HPM}" "${blacklist}" "${paralogs}" "${paralog_min_divergence}" $"{probe_HP_exons_split}" || exit 1
+python3 ParalogWizard_CastCreate.py "${path_to_data_HPM}" "${blacklist}" "${paralogs}" "${paralog_min_divergence}" "${probe_HP_exons_split}" || exit 1
 env echo
 
 env echo 'Copying results back to working directory'
